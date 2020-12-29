f12b35c571dab320bc9a884ae8b6025ed49522a4
Daniel Flanagan <daniel@lyte.dev>
2020-12-29 15:36:25 -0600

    Prod logging metadata

--- src/config/prod.exs
+++ src/config/prod.exs
@@ -6,4 +6,9 @@ config :lyric_screen, LyricScreen.Web.Endpoint,
 
 config :logger, level: :info
 
+config :logger, :console,
+	metadata: [:time, :level, :file, :function, :line, :mfa, :module, :pid, :request_id],
+	format: "$metadata\n$message\n\n",
+	colors: [enabled: true]
+
 import_config "prod.secret.exs"
