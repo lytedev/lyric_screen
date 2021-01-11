5e5f969914e0b29b328a9e831ec782c2c2e29959
Daniel Flanagan <daniel@lyte.dev>
2021-01-11 15:08:04 -0600

    Build and push versioned tags

--- src/priv/script/build-docker-image.sh
+++ src/priv/script/build-docker-image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
+v="$(cat mix.exs | grep '@version' | head -n 1 | cut -d '"' -f 2)"
 docker build \
 	--build-arg=GIT_REV="$(git rev-parse --short HEAD)" \
 	--build-arg=GIT_BRANCH="$(git branch --show-current)" \
-	--tag lytedev/lyric_screen:latest \
+	--tag lytedev/lyric_screen:"$v" \
 	--file src/priv/dockerfile .
+docker push lytedev/lyric_screen:"$v"
